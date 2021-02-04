04cce0793bb51968bb413660ef09db75f5dd9074
plikipe <plikipie@gmail.com>
2021-02-04 20:23:57 +0700

    styling overlay navbar fixed

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,10 +4,10 @@ const header = document.querySelector(".header");
 btnHamburger.addEventListener("click", function () {
   console.log("click hamburger");
   if (header.classList.contains("open")) {
-    //close hamburger menu
+    //close hamburger toggle
     header.classList.remove("open");
   } else {
-    //open hamburger menu
+    //open hamburger toggle
     header.classList.add("open");
   }
 });

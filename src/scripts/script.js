172c73d53ebf545eff9c318be24346f4227411d3
plikipe <plikipie@gmail.com>
2021-02-05 22:03:10 +0700

    fixed header navbar

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,13 +1,24 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", function () {
   console.log("click hamburger");
+
   if (header.classList.contains("open")) {
     //close hamburger toggle
     header.classList.remove("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
   } else {
     //open hamburger toggle
     header.classList.add("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
   }
 });

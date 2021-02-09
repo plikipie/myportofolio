d684bf89fc78e5d30c03bd5cfe39a8da0d5954e7
plikipe <plikipie@gmail.com>
2021-02-10 00:00:52 +0700

    change project

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,5 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade");
@@ -8,6 +9,7 @@ btnHamburger.addEventListener("click", function () {
 
   if (header.classList.contains("open")) {
     //close hamburger toggle
+    body.classList.remove("noscroll");
     header.classList.remove("open");
     fadeElems.forEach(function (element) {
       element.classList.remove("fade-in");
@@ -15,6 +17,7 @@ btnHamburger.addEventListener("click", function () {
     });
   } else {
     //open hamburger toggle
+    body.classList.add("noscroll");
     header.classList.add("open");
     fadeElems.forEach(function (element) {
       element.classList.remove("fade-out");
